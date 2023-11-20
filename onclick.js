e0519f127e3264b410b3912e57ab65c47eeefa1c
BrianAlano <145956836+BrianAlano@users.noreply.github.com>
2023-11-20 12:53:58 +0800

    Add files via upload

---
+++ onclick.js
@@ -0,0 +1,17 @@
+
+function image1()
+    {
+        window.alert("Thaiconcepts motorcycle");
+    }
+function image2()
+    {
+    window.alert("Thaiconcepts motorcycle");
+    }
+function image3()
+    {
+    window.alert("Thaiconcepts motorcycle");
+    }
+function image4()
+    {
+    window.alert("Thaiconcepts motorcycle");
+    }
